d10fd7c35ea22f4fa6adfeaba582bd203690d840
lcarv20 <lcarv20@gmail.com>
2020-07-01 16:33:14 +0200

    Fetch Data from DB and sending to user; Starting the post setting

--- routes/routes.js
+++ routes/routes.js
@@ -5,24 +5,40 @@ require("dotenv").config()
 //Date
 const d = new Date()
 const newDate = d.getDate() + "." + d.getMonth() + "." + d.getFullYear()
-const projectData = {
-	Date: newDate,
-	Temperature: "16",
-	Content: "Hellooo",
-}
+let projectData = [
+	{
+		City: "Vila Real",
+		Date: newDate,
+		Temperature: "16",
+		Content: "Hellooo",
+	},
+	{
+		City: "Porto",
+		Date: newDate,
+		Temperature: "23",
+		Content: "lkasdkbasjkdasjkd",
+	},
+]
 
 //OpenWeather API details
-const apiKey = process.env.API_KEY
-const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q="
-const units = "&units=metric"
+const api = {
+	key: process.env.API_KEY,
+	url: "https://api.openweathermap.org/data/2.5/weather?q=",
+	units: "&units=metric",
+}
 
 //Get routes
-router.get("/", (req, res) => {
+router.get("/db", (req, res) => {
 	res.send(projectData)
 })
 
-router.get("/db", (req, res) => {
-	res.send(projectData)
+router.get("/api", (req, res) => {
+	res.send(api)
+})
+//Post routes
+router.post("/postData", (req, res) => {
+	console.log(req.body)
+	projectData += req.body
 })
 
 module.exports = router
--- web/app.js
+++ web/app.js
@@ -1,4 +1,73 @@
-//Fetch all data (GET)
-const allEntries = fetch("/db")
-	.then((data) => data.json())
-	.then((data) => console.log(data))
+//Data Fetch
+const getData = async function (url) {
+	const connectDB = await fetch(url)
+		.then((data) => data.json())
+		.then((data) => data)
+
+	try {
+		console.log("Connection Successful:\n List of entries:")
+		connectDB.map((e) => {
+			console.log("->", e["City"])
+		})
+		return connectDB
+	} catch (error) {
+		document.getElementById("entryHolder").innerHTML =
+			"An error ocurred while loading ... please reload the page"
+		console.log("error", error)
+	}
+}
+
+getData("/db").then((data) => {
+	for (let entry of data) {
+		printData(entry)
+	}
+})
+
+//Data Printer
+function printData(entry) {
+	const target = document.getElementById("entryHolder")
+	const template = `
+    <div class="w3-third w3-center w3-padding w3-border w3-border-white w3-round-large">
+    <div id="City">Local: ${entry.City}</div>
+    <div id="date">Date: ${entry.Date}</div>
+    <div id="temp">Temperature: ${entry.Temperature}</div>
+    <div id="content">Content: ${entry.Content}</div>
+    </div>`
+
+	target.innerHTML += template
+}
+
+//post Data
+const apiData = async () => {
+	return await fetch("/api")
+		.then((api) => api.json())
+		.then((api) => console.log(api))
+}
+
+const postData = async function (url, data) {
+	console.log(data)
+	const method = {
+		method: "POST",
+		credentials: "same-origin",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	}
+	const response = await fetch(url, method)
+
+	try {
+		const newData = await response.json()
+		console.log(newData)
+	} catch (error) {
+		console.log("Error", error)
+	}
+}
+
+// postData("/postData", {
+// 	Braga: {
+// 		Date: 123545354,
+// 		Temperature: "16",
+// 		Content: "Hellooo",
+// 	},
+// })
